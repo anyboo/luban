6d199fe68d305b93d6c411868c48f1f956afd483
linzuxin <linzuxin@hotmail.com>
2017-05-12 10:30:14 +0800

    index空页面

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-import 'element-ui/lib/theme-default/index.css'
-import './styles/index.css'
-import './styles/btindex.css'
+import './styles/main.css'
 
 import Vue from 'vue'
 import VueResouse from 'vue-resource'
-import App from './app.vue'
+import App from './pages/app/app.vue'
 import router from './router'
-import store from './store'
-import BtCom from './components'
-import mixin from './mixins/mixin'
+//import store from './store'
+//import BtCom from './components'
+//import mixin from './mixins/mixin'
 
 Vue.use(VueResouse)
-Vue.use(BtCom)
-Vue.mixin(mixin)
+//Vue.use(BtCom)
+//Vue.mixin(mixin)
 
 new Vue({
     el: '#app',
-    store,
+   // store,
     router,
     render: h => h(App)
 })
---
+++ src/pages/app/app.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>鲁班1
+    </div>
+</template>
