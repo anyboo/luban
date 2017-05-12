4bb222105c7d4c07024fca61bd33166e43dc3d77
zhangyingyi <1142112046@qq.com>
2017-05-12 17:33:00 +0800

    commit studentInfo

---
+++ src/pages/students/studentInfo.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="app ng-scope app-header-fixed app-aside-fixed" :class="{'app-aside-folded':isSidebarCollapsed}">
+        <lb-header @sidebarCollapsed="sidebarCollapsed" />
+        <lb-sidebarmenu />
+        <div class="app-content ng-scope">
+            <router-view></router-view>
+        </div>
+        <lb-footer />
+    </div>
+</template>
