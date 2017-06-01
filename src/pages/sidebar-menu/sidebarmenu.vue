1aa700fb1ca280f8b76c959162947c35d8c841b3
linzuxin <linzuxin@hotmail.com>
2017-06-01 16:34:39 +0800

    提交滚动条美化

--- src/pages/sidebar-menu/sidebarmenu.vue
+++ src/pages/sidebar-menu/sidebarmenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="aside-wrap ng-scope">
         <div class="app-aside hidden-xs bg-black">
-            <div class="navi-wrap ps-container ps-theme-default">
+            <div class="navi-wrap ps-container ps-theme-default" :style="getClientHeight" @mouseout="handleMouseout" @mouseover="handleMouseove">
                 <nav class="navi clearfix ng-scope">
                     <ul class="nav ng-scope">
                         <template v-for="item in menu">
@@ -14,6 +14,24 @@
         </div>
     </div>
 </template>
+<style>
+::-webkit-scrollbar {
+    width: 10px !important;
+    background-color: #F5F5F5 !important;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+    background-color: #555;
+}
+
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+    background-color: #F5F5F5;
+}
+</style>
 <script>
 import sidebarmenuitem from '../sidebar-menu/sidebarmenuitem.vue'
 import homestat from '../homestat/homestat.vue'
@@ -24,15 +42,37 @@ export default {
         let lb_localdata = {}
         return {
             lb_localdata,
-            menu
+            menu,
+            clientHeight: '800px',
+            overflowy: 'hidden'
         }
+    },
+    mounted() {
+        this.clientHeight = document.body.clientHeight + 'px'
     },
     components: {
         'lb-sidebamenu-item': sidebarmenuitem,
         'lb-homestat': homestat
     },
-    computed: {},
+    computed: {
+        getClientHeight() {
+            return {
+                'max-height': this.clientHeight,
+                'overflow-y': this.overflowy
+            }
+        }
+    },
     watch: {},
-    methods: {}
+    methods: {
+        handleMouseout() {
+            this.clientHeight = document.body.clientHeight + 'px'
+            this.overflowy = 'hidden'
+        },
+        handleMouseove() {
+            this.clientHeight = document.body.clientHeight + 'px'
+            this.overflowy = 'auto'
+        },
+
+    }
 }
 </script>
