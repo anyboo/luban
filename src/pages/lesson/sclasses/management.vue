7d27cfd50822591a8705c1341f526f9e5e00e67f
zhangyingyi <1142112046@qq.com>
2017-06-05 16:25:05 +0800

    分配学员数据提交

---
+++ src/pages/lesson/sclasses/management.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="wrapper-xs ng-scope" xo-rest="packages" xo-rest-ctrl="scas_ss" xo-rest-grid="{maxsize:5,params:{pagesize:20,page:1,is_expired:0,assign_class:'0',ob_id:user.gv.ob_id,sj_id:vm.input.sj_id}}" loading-text="正在加载课时包..." empty-text="没有符合条件的课时包!">
+            <ul class="breadcrumb bg-white b-a">
+                <li><a ng-click="back_sclass_students()" class="ng-binding"><i class="fa fa-users"></i> 高中五班 学员管理</a></li>
+                <li class="active">分配学员</li>
+            </ul>
+            <div class="bg-light lter">
+                <p class="wrapper text-center text-muted ng-scope" ng-if="assign.students.length == 0">请在下面筛选并选择学员,点击选择</p>
+            </div>
+            <div class="row wrapper no-gutter bg-light">
+                <div class="col-xs-2">
+                    <p>筛选</p>
+                    <button class="btn btn-default btn-xs btn-info active" ng-class="{'btn-info':show_en_name,'active':show_en_name}" ng-click="toggle_en_name()" ui-toggle-class="btn-info">英文名</button>
+                </div>
+                <div class="col-xs-10">
+                    <div class="m-b-xs">
+                        <select class="form-control ng-pristine ng-untouched ng-valid" name="ol_id" ui-jq="chosen" ng-model="params.ol_id" ng-options="lesson.ol_id as lesson.lesson_name for lesson in $gv.lessons|filter:{'lesson_type':'10'}" style="display: none;">
+                            <option value="" class="">选择课程</option>
+                            <option value="0">基础民族舞蹈</option>
+                        </select>
+                        <div class="chosen-container chosen-container-single" style="width: 457px;" title=""><a class="chosen-single" tabindex="-1"><span>选择课程</span><div><b></b></div></a>
+                            <div class="chosen-drop">
+                                <div class="chosen-search">
+                                    <input type="text" autocomplete="off">
+                                </div>
+                                <ul class="chosen-results"></ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="m-b">
+                        <div class="input-group  ng-valid" select-title="请选择学员" ng-model="params.os_id" select-params="{ob_id:user.gv.ob_id}">
+                            <input type="text" placeholder="学员" ng-model="name" class="form-control ng-pristine ng-untouched ng-valid" ng-readonly="true" readonly="readonly"> <span class="input-group-btn"><button class="btn btn-default " select-tpl="tpl/directive/selectStudentTpl.html" select-id-field="os_id" max-num="1" on-selected="select_student" select-params="selectParams" select-title="请选择学员"><i class="icon-user"></i></button></span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-striped table-hover b-t b-light">
+                    <thead>
+                        <tr>
+                            <th>学员</th>
+                            <th>课程</th>
+                            <th>课次</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+                <div class="grid-data-result">
+                    <p class="text-center ng-binding ng-scope" ng-if="!loading &amp;&amp; grid.data.length==0"><i class="fa fa-frown-o"></i>没有符合条件的课时包!</p>
+                </div>
+            </div>
+            <div class="text-center">
+                <button class="btn btn-primary" ng-click="do_ok()" ng-disabled="assign.students.length == 0" disabled="disabled">
+                    确定</button>
+                <button class="btn btn-warning ng-click-active" @click="switchPage('lb-main')">返回</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'management',
+    data() {
+        let localdata = {}
+        return {
+            localdata,
+        }
+    },
+    computed: {},
+    watch: {},
+    methods: {
+        switchPage(page) {
+            if (this.$parent && this.$parent.switchPage) {
+                this.$parent.switchPage(page)
+            }
+        },
+    }
+}
+</script>
--- src/pages/lesson/sclasses/students_modal.vue
+++ src/pages/lesson/sclasses/students_modal.vue
@@ -6,56 +6,38 @@
                 <div ng-controller="SclassStudentsCtl" class="ng-scope">
                     <div class="modal-header">
                         <button class="close" type="button" @click="lbClosedialog($event)"><span aria-hidden="true">×</span><span class="sr-only">关闭</span></button>
-                        <h3 class="modal-title ng-binding"><i class="icon-user"></i>高中五班 学员管理</h3></div>
-                    <div class="modal-body ng-scope" ng-include="'tpl/app/lesson/sclasses/students_'+$action+'.block.html'">
-                        <div xo-rest="sclasses" xo-rest-url-param="[sclass.osc_id,'students']" xo-rest-grid="{maxsize:5,params:{pagesize:20,page:1}}" xo-rest-ctrl="sclass_students" class="table-responsive ng-scope">
-                            <p class="text-right" ng-hide="sclass.is_end == '1'"><a class="btn btn-primary btn-xs" ng-click="assign_students()"><i class="fa fa-plus"></i>分配学员</a></p>
-                            <div class="table-responsive">
-                                <table class="table table-striped b-t b-light">
-                                    <thead>
-                                        <tr>
-                                            <th ng-if="sclass.is_end == '0'" class="ng-scope"></th>
-                                            <th>学员</th>
-                                            <th>分配课次</th>
-                                            <th>已上课次</th>
-                                            <th>入班日期</th>
-                                        </tr>
-                                    </thead> 
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                                <div class="grid-data-result">
-                                    <p class="text-center ng-binding ng-scope" ng-if="!loading &amp;&amp; grid.data.length==0"><i class="fa fa-frown-o"></i>无相关数据!</p>
-                                </div>
-                            </div>
-                            <div class="panel-footer">
-                                <div class="row">
-                                    <div class="col-sm-4 col-xs-12"></div>
-                                    <div class="col-sm-4 text-center"><small class="text-muted inline m-t-sm m-b-sm ng-binding" ng-bind-template="共 0 名学员">共 0 名学员</small></div>
-                                    <div class="col-sm-4 text-right text-center-xs">
-                                        <ul class="pagination-sm m-t-none pagination ng-isolate-scope ng-valid" total-items="grid.total" ng-model="grid.params.page" max-size="grid.maxsize" items-per-page="grid.params.pagesize" boundary-links="true" rotate="false">
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <h3 class="modal-title ng-binding"><i class="icon-user"></i>高中五班 学员管理</h3>
                     </div>
+                    <component v-bind:is="currentView">
+                    </component>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import main from './main.vue'
+import management from './management.vue'
 export default {
     name: 'students_modal',
     data() {
         let localdata = {}
         return {
             localdata,
+            currentView: 'lb-main'
         }
+    },
+    components: {
+        'lb-main': main,
+        'lb-management': management
+
     },
     computed: {},
     watch: {},
-    methods: {}
+    methods: {
+        switchPage(page) {
+            this.currentView = page
+        }
+    }
 }
 </script>
