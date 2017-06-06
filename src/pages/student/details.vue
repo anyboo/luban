5f941849845b58df5be228a3a06d637556567de1
zhangyingyi <1142112046@qq.com>
2017-06-06 18:33:31 +0800

    订单提交

--- src/pages/student/details.vue
+++ src/pages/student/details.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-        <div class="modal-dialog" ng-class="{'modal-sm': size == 'sm', 'modal-lg': size == 'lg','modal-full':size == 'full'}">
-            <div class="modal-content" modal-transclude="">
-                <div class="panel panel-default ng-scope">
-                    <div class="panel-heading ng-binding" v-model="localdata.form.match_name">qq1dd参加wqsedqwd比赛 详情</div>
-                    <div class="panel-body">
-                        <ul class="list-group">
-                            <li class="list-group-item ng-binding">参赛日期:2017年00月DD日</li>
-                            <li class="list-group-item ng-binding" v-model="localdata.form.result">比赛成绩:999</li>
-                        </ul>
-                        <div class="wrapper ng-binding" ng-bind-html="vm.input.match.detail"></div>
-                    </div>
-                    <div class="panel-footer text-center"><a class="btn btn-default" @click="lbClosedialog($event)">关闭</a></div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="ng-scope">
+                <div class="modal-header">
+                    <button class="close" type="button" @click="lbClosedialog($event)">
+                        <span>×</span>
+                        <span class="sr-only">关闭</span>
+                    </button>
+                    <h3 class="modal-title">
+                        <i class="fa fa-user"></i>编辑
+                        <span class="badge bg-info ng-binding">{{localdata.form.student_name }}</span>比赛 详情
+                    </h3>
+                </div>
+                <div class="modal-body">
+                    <ul class="list-group">
+                        <li class="list-group-item ng-binding">参赛日期:2017年00月DD日</li>
+                        <li class="list-group-item ng-binding" v-model="localdata.form.result">比赛成绩:999</li>
+                    </ul>
+                    <div class="wrapper ng-binding" ng-bind-html="vm.input.match.detail"></div>
+                </div>
+                <div class="modal-footer text-center">
+                    <button class="btn btn-primary"  @click="lbClosedialog($event)">确定</button>
                 </div>
             </div>
         </div>
@@ -24,9 +33,9 @@ export default {
         let localdata = {
             'form': {
                 'match_name': '',
-                'result':''
+                'result': ''
 
-              
+
             },
         }
         return {
--- src/pages/student/recording.vue
+++ src/pages/student/recording.vue
@@ -1,58 +1,43 @@
 <template>
     <div>
-        <div class="panel panel-default">
-            <div class="panel-heading" tab-link="student.list"><span class="tab-title"><i class="fa fa-flag-o"></i> 赛事记录</span>
-                <div class="dropdown pull-right" dropdown=""><span tooltip="更多" dropdown-toggle="" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-align-justify"></i></span>
-                    <ul class="dropdown-menu">
-                        <li ui-per="student.trials.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope" style="display: none;"><a ui-sref="student.view.trials({os_id:os_id})" class="ng-binding" href="#/student/65144/trials"><i class="icon-chemistry"></i> 试课记录</a></li>
-                        <li ui-per="student.evaluates.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope" style="display: none;"><a ui-sref="student.view.evaluates({os_id:os_id})" class="ng-binding" href="#/student/65144/evaluates"><i class="fa fa-user-md"></i> 评估记录</a></li>
-                        <li ui-per="student.lessons" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.lessons({os_id:os_id})" class="ng-binding" href="#/student/65144/lessons"><i class="fa fa-book"></i> 课程记录</a></li>
-                        <li ui-per="student.pays.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.pays({os_id:os_id})" class="ng-binding" href="#/student/65144/pays"><i class="fa fa-money"></i> 缴费记录</a></li>
-                        <li ui-per="student.tracks.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.tracks({os_id:os_id})" class="ng-binding" href="#/student/65144/tracks"><i class="fa fa-comments"></i> 跟踪回访记录</a></li>
-                        <li ui-per="student.arranges.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.arranges({os_id:os_id})" class="ng-binding" href="#/student/65144/arranges"><i class="fa fa-calendar"></i> 排课记录</a></li>
-                        <li ui-per="student.attendances.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.attendances({os_id:os_id})" class="ng-binding" href="#/student/65144/attendances"><i class="fa fa-calendar-o"></i> 考勤记录</a></li>
-                        <li ui-per="student.leaves.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.leaves({os_id:os_id})" class="ng-binding" href="#/student/65144/leaves"><i class="fa fa-bell-o"></i> 请假记录</a></li>
-                        <li ui-per="student.comments.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.comments({os_id:os_id})" class="ng-binding" href="#/student/65144/comments"><i class="fa fa-comments"></i> 点评记录</a></li>
-                        <li ui-per="student.performance.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.performance({os_id:os_id})" class="ng-binding" href="#/student/65144/performance"><i class="fa fa-star"></i> 成绩记录</a></li>
-                        <li ui-per="student.match.list" ng-repeat="item in $const.student_view_tabs" class="ng-scope"><a ui-sref="student.view.matchs({os_id:os_id})" class="ng-binding" href="#/student/65144/matchs"><i class="fa fa-flag-o"></i> 赛事记录</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="panel-body ng-scope" ui-view="">
-                <div xo-rest="students" xo-rest-url-param="[$stateParams.os_id,'match']" xo-rest-grid="{maxsize:5,params:{pagesize:20,page:1}}" xo-rest-ctrl="student_matches" loading-container=".table-responsive" loading-text="正在加载赛事记录..." empty-text="没有赛事记录!" class="ng-scope">
-                    <a class="btn btn-default m-b" @click="handleShowDialog('lb-regstudentmatchmodal')"><i class="fa fa-plus">
-                    
+        <div ui-view class="ng-scope">
+            <div class="wrapper-xs ng-scope">
+                <div class="panel panel-default">
+                    <div class="panel-body ng-scope" ui-view="">
+                        <div xo-rest="students" xo-rest-url-param="[$stateParams.os_id,'match']" xo-rest-grid="{maxsize:5,params:{pagesize:20,page:1}}" xo-rest-ctrl="student_matches" loading-container=".table-responsive" loading-text="正在加载赛事记录..." empty-text="没有赛事记录!" class="ng-scope">
+                            <a class="btn btn-default m-b" @click="handleShowDialog('lb-regstudentmatchmodal')"><i class="fa fa-plus">                   
                 </i> 添加赛事记录</a>
-                    <div class="table-responsive">
-                        <table class="table table-striped b-t b-light">
-                            <thead>
-                                <tr>
-                                    <th>日期</th>
-                                    <th>赛事名称</th>
-                                    <th>比赛成绩</th>
-                                    <th>录入时间</th>
-                                    <th>操作</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <template v-for="item in getTablesData()">
-                                    <tr ng-repeat="item in grid.data" ng-if="!loading" class="ng-scope">
-                                        <td class="ng-binding">{{item.join_date}}</td>
-                                        <td class="ng-binding">{{item.match_name}}</td>
-                                        <td class="ng-binding">{{item.result}}</td>
-                                        <td class="ng-binding">2017-06-06</td>
-                                        <td><a class="btn btn-default btn-xs" @click="handleShowDialog('lb-details')">查看详情</a> <a class="btn btn-danger btn-xs ng-isolate-scope" @click="handleDelClick(item._id)">删除</a></td>
-                                    </tr>
-                                </template>
-                            </tbody>
-                        </table>
-                        <div class="panel-footer ">
-                        <div class="row ">
-                            <lb-pagination class="pull-right" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagination.currentPage" :page-sizes="pagination.pagesizes" :page-size="pagination.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="pagination.total">
-                            </lb-pagination>
-                        </div>
-                    </div>
-                        <div class="grid-data-result">
+                            <div class="row">
+                                <lb-table :data="getTablesData()" stripe>
+                                    <lb-table-column prop="data" label="日期">
+                                        <template scope="scope">
+                                            {{scope.row.join_date}}
+                                        </template>
+                                    </lb-table-column>
+                                    <lb-table-column prop="data" label="赛事名称">
+                                        <template scope="scope"> {{scope.row.match_name}}</template>
+                                    </lb-table-column>
+                                    <lb-table-column prop="data" label="比赛成绩">
+                                        <template scope="scope"> {{scope.row.result}}</template>
+                                    </lb-table-column>
+                                    <lb-table-column prop="data" label="录入时间">
+                                        <template scope="scope">
+                                            2017-06-06
+                                        </template>
+                                    </lb-table-column>
+                                    <lb-table-column prop="data" label="操作">
+                                        <template scope="scope"><a class="btn btn-default btn-xs" @click="handleShowDialog('lb-details')">查看详情</a> <a class="btn btn-danger btn-xs ng-isolate-scope" @click="handleDelClick(scope.row._id)">删除</a></template>
+                                    </lb-table-column>
+                                </lb-table>
+                                <div class="panel-footer ">
+                                    <div class="row ">
+                                        <lb-pagination class="pull-right" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagination.currentPage" :page-sizes="pagination.pagesizes" :page-size="pagination.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="pagination.total">
+                                        </lb-pagination>
+                                    </div>
+                                </div>
+                                <div class="grid-data-result">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
