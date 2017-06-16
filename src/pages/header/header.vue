5b1975e0941c86f60a8ab7bf0c03f879153d46de
zhangyingyi <1142112046@qq.com>
2017-06-16 17:06:26 +0800

    全屏提交

--- src/pages/header/header.vue
+++ src/pages/header/header.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="collapse pos-rlt navbar-collapse box-shadow bg-info dker">
             <div class="nav navbar-nav hidden-xs">
-                <a @click="sidebarCollapsed" class="btn no-shadow navbar-btn">
+                <a @click="variety()" class="btn no-shadow navbar-btn">
                     <i class="fa fa-fw fa-dedent"></i>
                 </a>
             </div>
@@ -85,8 +85,8 @@ export default {
     computed: {},
     watch: {},
     methods: {
-        sidebarCollapsed() {
-            this.$emit('sidebarCollapsed')
+        variety() {
+            this.$emit('variety')
         },
         operating() {
             this.open = !this.open
--- src/pages/sidebar-menu/sidebarmenuitem.vue
+++ src/pages/sidebar-menu/sidebarmenuitem.vue
@@ -1,16 +1,16 @@
 <template>
     <li @click="handleClick" class="ng-scope" :class="{'active':active}">
-        <a class="auto ng-scope" @click="handleRouter($event,menu)">
-            <span class="pull-right text-muted" v-if="menu.menu">
+        <a class="auto ng-scope active" @click="handleRouter($event,menu)">
+            <span class="pull-right text-muted active" v-if="menu.menu">
                 <i class="fa fa-fw fa-angle-right text"></i>
-                <i class="fa fa-fw fa-angle-down text-active"></i>
+                <i class="fa fa-fw fa-angle-down text-active active"></i>
             </span>
             <i class="fa fa-users" :class="menu.menuIcon" v-if="menu.menuIcon"></i>
             <span class="font-normal ng-binding">{{menu.menuTitle}}</span>
         </a>
-        <ul class="nav nav-sub dk ng-scope">
+        <ul class="nav nav-sub dk ng-scope active" style="top: auto; bottom: auto;">
             <template v-for="item in menu.menu">
-                <li class="ng-scope">
+                <li class="ng-scope active">
                     <a @click="handleRouter($event,item)">
                         <span class="ng-binding">{{item.menuTitle}}</span>
                     </a>
@@ -27,7 +27,7 @@ export default {
         let localdata = {}
         return {
             localdata,
-            active: false
+            active: true
         }
     },
     computed: {},
