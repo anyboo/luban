185cffa480ed7559c0ce98509865000c85b29f3c
zhangyingyi <1142112046@qq.com>
2017-06-28 11:10:51 +0800

    底部

--- luban/src/pages/app/body.vue
+++ luban/src/pages/app/body.vue
@@ -1,23 +1,42 @@
 <template>
-    <div class="grid">
-        <div class="grid-item">...</div>
-        <div class="grid-item grid-item--width2">...</div>
-        <div class="grid-item">...</div>
+    <div class="grid-stack">
+   
     </div>
 </template>
+ <style>
+.grid-stack {
+    height: 800px;
+    z-index: 101;
+}
+
+.grid-stack-placeholder .grid-stack-item-content {
+    background: #C90;
+    z-index: 2;
+}
+
+.grid-stack-item-content:hover {
+    border-color: hsla(0, 0%, 100%, 0.5);
+    cursor: move;
+}
+
+.grid-stack-item-content {
+    background: #C09;
+    border: 2px solid hsla(0, 0%, 0%, 0.5);
+}
+</style>
 <script>
 export default {
-   name: 'login',
+    name: 'login',
     data() {
         let localdata = {}
         return {
             localdata
         }
     },
+    mounted() {
+     
+    },
     methods: {
-        handleClick(action){
-            this.$emit('viewChange',action)
-        }
     }
 }
 </script>
