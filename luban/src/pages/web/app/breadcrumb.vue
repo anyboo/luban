2588a5a0dd49277bef018d17e504f7530a46cc3a
sanjin <linzuxin@hotmail.com>
2017-07-05 10:13:31 +0800

    添加学员

--- luban/src/pages/web/app/breadcrumb.vue
+++ luban/src/pages/web/app/breadcrumb.vue
@@ -20,6 +20,7 @@ export default {
         getCurrMenu() {
             var menuName = ''
             let to = this.$route.path
+            console.log(to)
             for (var item of menu) {
                 if (item.to == to) {
                     menuName = item.menuTitle
@@ -34,6 +35,9 @@ export default {
                         }
                     }
                 }
+            }
+            if (to=='/'||to=='/web'){
+                menuName = '档案录入'
             }
             return menuName
         }
