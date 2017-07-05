8944d1d61eb6a204c8de112d20a62fe859afa8bd
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-07-05 12:43:55 +0800

    s

--- luban/src/pages/window/app/body.vue
+++ luban/src/pages/window/app/body.vue
@@ -1,48 +1,30 @@
 <template>
-    <div>
-        <template v-for="item,index in new Array(8)">
-            <div class="hte-shortcut ui-draggable ui-draggable-handle ui-droppablea b">
-                <div class="hte-shortcut-icon"><img src="/assets/images/logo.png"></div>
-                <div class="hte-shortcut-name">快递100</div>
-            </div>
-        jjj
-            {{item}}
-        </template> 
+    <div class='margins-tops'>
+        <template v-for="item in menu">
+            <lb-icon :icondata="item"></lb-icon>
+        </template>
     </div>
 </template>
 <style>
-.hte-shortcut-icon img {
-    width: 40px;
-    height: 40px;
-    margin-top: 20px;
-    margin-left: 20px;
-}
-
-.hte-shortcut-name {
-    margin-top: 20px;
-    margin-left: 20px;
-}
-
-.hte-shortcut .hte-shortcut-name {
-    margin-top: 5px;
-    margin-left: 16px;
-    font-size: #fff;
+.margins-tops {
+    margin-top: 4.3rem;
 }
 </style>
 <script>
+import menu from '~/stores/menu'
+import icon from './icon.vue'
 export default {
     name: 'login',
     data() {
         let localdata = {}
         return {
             localdata,
-            Array: ''
+            Array: '',
+            menu
         }
     },
-
-    mounted() {
-
-    },
-    methods: {}
+    components: {
+        'lb-icon': icon
+    }
 }
 </script>
---
+++ luban/src/pages/window/app/icon.vue
@@ -0,0 +1,81 @@
+<template >
+    <div class="shortcut-icon" @mousedown='handleMousedown($event)'>
+        <div class="shortcut-img">
+            <i :style="{'color': $store.state.system.theme}" class="fa fa-2x" :class="icondata.menuIcon"></i>
+        </div>
+        <div class="shortcut-name">{{icondata.menuTitle}}</div>
+    </div>
+</template>
+ <style>
+.shortcut-icon {
+    width: 80px;
+    height: 80px;
+    padding: 10px;
+    line-height: 35px;
+    position: absolute;
+    border: 1px solid transparent
+}
+
+div.shortcut-icon:hover {
+    border: 1px solid rgba(65, 152, 235, 0.6);
+    background: rgba(65, 152, 235, 0.3)
+}
+
+.shortcut-img {
+    text-align: center;
+}
+
+.shortcut-name {
+    margin-top: 4px;
+    text-align: center;
+    color: #fff;
+    font-size: 18px;
+}
+</style>
+ <script>
+
+export default {
+    name: 'icon',
+    props: ['icondata'],
+    data() {
+        let localdata = {}
+        return {
+            localdata,
+            disx: 0,
+            disy: 0,
+        }
+    },
+    methods: {
+        addListener(element, e, fn) {
+            element.addEventListener ? element.addEventListener(e, fn, false) : element.attachEvent("on" + e, fn)
+        },
+        removeListener(element, e, fn) {
+            element.removeEventListener ? element.removeEventListener(e, fn, false) : element.detachEvent("on" + e, fn)
+        },
+        handleMousedown: function (ev) {
+            console.log('handleMousedown')
+            this.disx = ev.clientX - this.$el.offsetLeft || 0
+            this.disy = ev.clientY - this.$el.offsetTop || 0
+            this.addListener(window, 'blur', this.handleMouseUp)
+            this.addListener(document, 'mousemove', this.handleMouseMove)
+            this.addListener(document, 'mouseup', this.handleMouseUp)
+        },
+        handleMouseMove(ev) {
+            console.log('handleMouseMove')
+            let i_x = ev.clientX - this.disx
+            let i_y = ev.clientY - this.disy
+            this.$el.style.left = Math.max(i_x, 0) + 'px'
+            this.$el.style.top = Math.max(i_y, 0) + 'px'
+        },
+        handleMouseUp(ev) {
+            console.log('handleMouseUp')
+            this.removeListener(document, 'mousemove', this.handleMouseMove)
+            this.removeListener(document, 'mouseup', this.handleMouseUp)
+            this.removeListener(window, "blur", this.handleMouseUp)
+        },
+    }
+}
+</script>
+ 
+
+        
