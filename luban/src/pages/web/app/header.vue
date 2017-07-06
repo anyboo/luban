4b0b37074c7f1d50b931f47346205fe7f46825b6
zhangyingyi <1142112046@qq.com>
2017-07-06 14:13:14 +0800

    d

--- luban/src/pages/web/app/header.vue
+++ luban/src/pages/web/app/header.vue
@@ -10,7 +10,7 @@
                     </span>
                 </a>
             </div>
-            <div class="topbar-main" style="position:relative;">
+            <div class="topbar-main">
                 <a id="menu-toggle" href="#" class="hidden-xs" @click="variety()">
                     <i class="fa fa-bars"></i>
                 </a>
@@ -20,7 +20,7 @@
                             <i class="fa fa-fw" :class="{'fa-compress':updown,'fa-expand':!updown}"></i>
                         </a>
                     </li>
-                    <li class="dropdown topbar-user open"style="position:relative;">
+                    <li class="dropdown topbar-user open">
                         <a data-hover="dropdown" href="#" class="dropdown-toggle" @click="cut" @blur="blur">
                             <img src="/assets/images/a0.jpg" alt="" class="img-responsive img-circle">&nbsp;
                             <span class="hidden-xs">林祖鑫</span>&nbsp;
@@ -46,9 +46,6 @@
         </nav>
     </div>
 </template>
-<style>
-    
-</style>
 <script>
 export default {
     name: 'Topbar',
@@ -57,7 +54,6 @@ export default {
             hide: false,
             updown: false,
             menu: false,
-
         }
     },
     methods: {
