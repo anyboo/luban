ac449dfb6bde25f8ec3596e59f9863121e40d39d
zhangyingyi <1142112046@qq.com>
2017-07-07 16:01:17 +0800

    编辑

--- luban/src/pages/views/system/personal_information.vue
+++ luban/src/pages/views/system/personal_information.vue
@@ -13,7 +13,6 @@
                                     <div class="m-b-xs m-t-sm">
                                         <span class="h3 text-black ng-binding ng-scope" ng-if="user.name">张英乙</span>
                                     </div>
-                                    <button class="btn btn-default btn-xs" ng-click="$util.open('tpl/main/photo.modal.html',lg,user)">更换图像</button>
                                 </div>
                             </div>
                         </div>
@@ -27,39 +26,20 @@
                         </dl>
                         <dl class="dl-horizontal b-b padder-v">
                             <dt>账号密码</dt>
-                            <dd>********* <a class="link pull-right" @click="status=3">修改密码 </a>
-                                <p class="text-muted">修改密码请点击右边"修改密码"链接</p> 
+                            <dd>********* <a class="link pull-right" @click="status=3" style="height:31px;">修改密码</a>
+                                <p class="text-muted">修改密码请点击右边"修改密码"链接</p>
                             </dd>
                         </dl>
                         <dl class="dl-horizontal b-b padder-v">
                             <dt>真实姓名</dt>
-                            <dd class="ng-binding">张英乙 <a class="link pull-right" @click="status=1">修改姓名</a>
+                            <dd class="ng-binding" style="height:31px;">张英乙 <a class="link pull-right" @click="status=1">修改姓名</a>
                                 <p class="text-muted">修改姓名请点击右边"修改姓名"" 链接</p>
                             </dd>
                         </dl>
                         <dl class="dl-horizontal b-b padder-v">
                             <dt>生日</dt>
-                            <dd class="ng-binding">- <a class="link pull-right" @click="status=2">设置生日</a>
+                            <dd class="ng-binding" style="height:31px;">- <a class="link pull-right" @click="status=2">设置生日</a>
                                 <p class="text-muted">设置出生日期请点击右边"设置生日" 链接</p>
-                            </dd>
-                        </dl>
-                        <dl class="dl-horizontal b-b padder-v">
-                            <dt><i class="fa fa-mobile"></i> 手机绑定</dt>
-                            <dd>
-                                <p class="text-success ng-binding ng-scope" ng-if="profile.is_mobile_bind == '1'">已绑定手机号 15080555304</p>
-                            </dd>
-                        </dl>
-                        <dl class="dl-horizontal b-b padder-v" @click="status=5">
-                            <dt><i class="fa fa-envelope"></i> 邮箱绑定</dt>
-                            <dd>
-                                <p class="text-muted ng-scope" ng-if="profile.is_email_bind != '1'">未绑定邮箱 <a class="btn btn-success btn-sm pull-right" ng-click="bind_email()"><i class="fa fa-envelope"></i> 绑定邮箱</a></p>
-                            </dd>
-                        </dl>
-                        <dl class="dl-horizontal b-b padder-v" @click="status=6">
-                            <dt><i class="fa fa-weixin"></i> 微信绑定</dt>
-                            <dd>
-                                <p class="text-muted ng-scope" ng-if="profile.is_weixin_bind != '1'">未绑定微信号 <a class="btn btn-success btn-sm pull-right" ng-click="bind_weixin()"><i class="fa fa-weixin"></i> 绑定微信</a></p>
-                                <p class="text-muted">绑定微信之后，可以用微信登录本系统，还可以接收微信通知.</p>
                             </dd>
                         </dl>
                     </div>
@@ -91,36 +71,6 @@
                                 <button class="btn btn-primary" type="submit">修改密码</button> <a class="link m-l" @click="status=0">返回</a></div>
                         </div>
                     </form>
-                </div>
-                <div class="wrapper-md m-t" v-if="status==5">
-                    <form name="bind_email_form" class="form-horizontal form-validation ng-pristine ng-scope ng-invalid ng-invalid-required ng-valid-email" ng-switch-when="bind_email" novalidate="">
-                        <h4 class="b-b m-b padder-v">绑定邮箱</h4>
-                        <div class="form-group">
-                            <label class="control-label col-xs-12 col-md-2">电子邮箱:</label>
-                            <div class="col-xs-12 col-md-3">
-                                <div class="input-group">
-                                    <input type="email" ng-model="bind.email.email" placeholder="输入您的常用邮箱" class="form-control ng-pristine ng-untouched ng-valid-email ng-invalid ng-invalid-required" required=""> <span class="input-group-btn"><button ng-disabled="bind_email_form.email.$invalid || saving" class="btn btn-default ng-binding" ng-click="get_email_vcode()">获取验证码</button></span></div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-xs-12 col-md-2">验证码:</label>
-                            <div class="col-xs-12 col-md-3">
-                                <input type="text" ng-model="bind.email.vcode" placeholder="输入收到的验证码" class="form-control ng-pristine ng-untouched ng-invalid ng-invalid-required" required="">
-                            </div>
-                        </div>
-                        <div class="form-group m-t">
-                            <div class="col-xs-12 col-md-offset-2">
-                                <button class="btn btn-primary" type="submit" ng-disabled="bind_email_form.$invalid || saving" ng-click="bind_do('email')" disabled="disabled">确认绑定</button> <a class="link m-l" @click="status=0">返回</a></div>
-                        </div>
-                    </form>
-                </div>
-                <div class="wrapper-md m-t" ng-switch="$action" v-if="status==6">
-                    <div class="wrapper ng-scope" ng-switch-when="bind_weixin">
-                        <h4 class="b-b m-b padder-v">微信绑定</h4><img class="center-block" ng-src="/qr/index?u=ogabind&amp;t=aa1543fce22a54a24bdea90a9ab45931" src="/assets/images/index.png">
-                        <p class="text-center ng-scope" ng-if="bind_step == 0">
-                            <span ng-if="!app.isWeiXin" class="ng-scope">请使用微信扫一下二维码</span><a class="link m-l" @click="status=0">返回</a>
-                        </p>
-                    </div>
                 </div>
                 <div class="wrapper-md m-t" v-if="status==1">
                     <form name="edit_name_form" class="form-horizontal form-validation ng-pristine ng-valid ng-scope ng-valid-required ng-valid-parse" ng-switch-when="edit_name" novalidate="">
@@ -158,6 +108,13 @@
         </div>
     </div>
 </template>
+<style >
+    a.link {
+    color: #4d90fe;
+    position: relative;
+    left: -54px;
+}
+</style>
 <script>
 export default {
     name: 'personal_information',
