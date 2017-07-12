83f5857ab52b58693c13a799775ca9bee41d1338
sanjin <linzuxin@hotmail.com>
2017-07-12 11:52:05 +0800

    s

---
+++ luban/src/router/index.js
@@ -0,0 +1,18 @@
+var routes = []
+import web from '~/pages/web/app/app.vue'
+import windowweb from '~/pages/window/app/app.vue'
+
+routes.push({
+    path: '/window',
+    component: windowweb
+})
+routes.push({
+    path: '/web',
+    component: web
+})
+routes.push({
+    path: '*',
+    component: web
+})
+
+export default routes
