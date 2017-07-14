d0a2645f6d96d0751fb35ecd1981abc55ac8bfc5
sanjin <linzuxin@hotmail.com>
2017-07-14 10:38:20 +0800

    demo数据库提交

--- backend/api/controllers/control.js
+++ backend/api/controllers/control.js
@@ -28,6 +28,16 @@ function changeModelId(model) {
     }
 }
 
+module.exports.login = function* login(next) {
+    if ('POST' != this.method) return yield next
+    var model = yield parse(this, {
+        limit: '500kb'
+    })
+    console.log(model)
+    this.body = {
+        token: '123'
+    }
+}
 module.exports.all = function* all(name, next) {
     if ('GET' != this.method) return yield next
     let query = this.query
@@ -77,13 +87,17 @@ module.exports.all = function* all(name, next) {
         }
     }
     changeModelId(findObj)
-    let count = yield wrap(db.get(name)).count(findObj)
+    console.log(findObj, name)
+    let table = wrap(db.get(name))
+    console.log('yield', table)
+    let count = yield table.count(findObj)
+    console.log(options, name)
     options.push({ '$match': findObj })
     options.push({ '$sort': { '_id': -1 } })
     options.push({ '$skip': skip })
     options.push({ '$limit': limit })
     console.log(options, name)
-    let data = yield wrap(db.get(name)).aggregate(options)
+    let data = yield table.aggregate(options)
     this.body = yield {
         'data': data,
         'count': count,
