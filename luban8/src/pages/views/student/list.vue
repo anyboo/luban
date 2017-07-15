5af10c40d09de39488b0e477c56dec55ad048f1a
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-07-15 14:50:22 +0800

    优化的组件模板

--- luban8/src/pages/views/student/list.vue
+++ luban8/src/pages/views/student/list.vue
@@ -1,137 +1,113 @@
 <template>
     <div class="wrapper">
-        <div class=" wrapper panel panel-default ng-scope">
-            <div class="row wrapper">
-                <div class="col-xs-12 col-md-4 m-t">
-                    <div class="padder">
-                        <div class="input-group w-full">
-                            <div class="input-group">
-                                <div class="input-group-btn">
-                                    <el-dropdown menu-align="start" @command="handleCommand">
-                                        <lb-dropdown-button class="btn btn-default btn-sm ng-pristine ng-valid ng-touched">
-                                            {{localdata.search.search_value}}
-                                            <span class="caret"></span>
-                                        </lb-dropdown-button>
-                                        <el-dropdown-menu slot="dropdown" style="z-index:3000;">
-                                            <template v-for="item in localdata.search.fields">
-                                                <el-dropdown-item :command="item.name">{{item.value}}</el-dropdown-item>
-                                            </template>
-                                        </el-dropdown-menu>
-                                    </el-dropdown>
-                                </div>
-                                <input type="text" class="input-sm form-control ng-pristine ng-untouched ng-valid" placeholder="关键字" v-model.lazy="localdata.form.search_value" @change="handleSearch">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-sm btn-default" type="button" @click="handleSearch">搜索</button>
-                                </span>
-                            </div>
+        <div class="row wrapper">
+            <div class="col-xs-12 col-md-4 m-t">
+                <div class="input-group w-full">
+                    <div class="input-group">
+                        <div class="input-group-btn">
+                            <el-dropdown menu-align="start" @command="handleCommand">
+                                <lb-dropdown-button class="btn btn-default btn-sm ">
+                                    {{localdata.search.search_value}}
+                                    <span class="caret"></span>
+                                </lb-dropdown-button>
+                                <el-dropdown-menu slot="dropdown" style="z-index:3000;">
+                                    <template v-for="item in localdata.search.fields">
+                                        <el-dropdown-item :command="item.name">{{item.value}}</el-dropdown-item>
+                                    </template>
+                                </el-dropdown-menu>
+                            </el-dropdown>
                         </div>
+                        <input type="text" class="input-sm form-control " placeholder="关键字" v-model.lazy="localdata.form.search_value" @change="handleSearch">
+                        <span class="input-group-btn">
+                            <button class="btn btn-sm btn-default" type="button" @click="handleSearch">搜索</button>
+                        </span>
                     </div>
                 </div>
-                <div class="col-xs-12 col-md-8 m-t">
-                    <lb-buttongroup :group-data="localdata.status" v-model="localdata.form.status" @input="handleSearch"></lb-buttongroup>
-                    <lb-buttongroup :group-data="localdata.view_mode" v-model="localdata.form.view_mode"></lb-buttongroup>
+            </div>
+            <div class="col-xs-12 col-md-8 m-t">
+                <lb-buttongroup :group-data="localdata.status" v-model="localdata.form.status" @input="handleSearch"></lb-buttongroup>
+                <lb-buttongroup :group-data="localdata.view_mode" v-model="localdata.form.view_mode"></lb-buttongroup>
     
-                    <a @click="lbShowdialog($event,'lb-trash')" :class="{result:refreshData}">
-                        <i class="icon-lock"></i>已封存档案
-                    </a>
-                </div>
-            </div>
-            <div class=" list-student ">
-                <div class="row ng-scope" v-if="localdata.form.view_mode == 'image'">
-                    <template v-for="item in getTablesData()">
-                        <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6 m-b ng-scope">
-                            <div class="bg-white b-a r-2x list-student-item box-shadow">
-                                <div class="face ng-scope">
-                                    <a @click="handleRouter($event,item)">
-                                        <div class="avatar-wrapper adres-css" style="border-radius:0; display:block; overflow:hidden;border-radius: 80px; width:80px; height:80px; ">
-                                            <img :src="makeImage(item.student_name,80)" style="vertical-align:top;" width="100%" height="">
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="name m-t">
-                                    <a class="link ng-binding" @click="handleRouter($event,item)">
-                                        <span class="ng-binding">
-                                            <i class="fa" :class="{'fa-female':item.sex=='0','fa-male':item.sex!='0'}"></i>
-                                        </span>{{ item.student_name }}
-                                        <span v-if="item.nickname != ''" class="ng-binding ng-scope">{{ item.nickname }}</span>
-                                    </a>
-                                </div>
-                                <div class="tel m-t">
-                                    <i class="fa fa-phone"></i>
-                                    <span class="ng-binding">{{ item.first_tel }}</span>
-                                    <lb-dropdown :drop-menu-data="localdata.dropDownMenu" :menu-data="item" class="pull-right">
-                                        <lb-dropdown-button slot="buttonslot" button-class="btn btn-info btn-xs" button-tooltip="操作">
-                                            <i class="fa fa-cog ng-scope"></i>
-                                            <span class="caret"></span>
-                                        </lb-dropdown-button>
-                                    </lb-dropdown>
-                                </div>
-                            </div>
-                        </div>
-                    </template>
-                </div>
-                <div class="row ng-scope" v-if="localdata.form.view_mode == 'list'">
-                    <el-table :data="getTablesData()" stripe>
-                        <el-table-column prop="data" label="操作">
-                            <template scope="scope">
-                                <lb-dropdown :drop-menu-data="localdata.dropDownMenu" :menu-data="scope.row">
-                                    <lb-dropdown-button slot="buttonslot" button-class="btn btn-info btn-xs" button-tooltip="操作">
-                                        <i class="fa fa-cog ng-scope"></i>
-                                        <span class="caret"></span>
-                                    </lb-dropdown-button>
-                                </lb-dropdown>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="学员">
-                            <template scope="scope">
-                                <a class="link ng-binding" @click="handleRouter($event,scope.row)">
-                                    <span class="ng-binding">
-                                        <i class="fa" :class="{'fa-female':scope.row.sex=='2','fa-male':scope.row.sex=='1'}"></i>
-                                    </span>{{ scope.row.student_name }}
-                                    <span v-if="scope.row.nickname != ''" class="ng-binding ng-scope">{{ scope.row.nickname }}</span>
-                                </a>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="联系电话">
-                            <template scope="scope">
-                                <span class="ng-binding">{{ scope.row.first_tel }}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="年龄">
-                            <template scope="scope">
-                                <span class="ng-binding ng-scope">{{ fromNow(scope.row.birth) }}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="住址">
-                            <template scope="scope">{{ scope.row.home_address }}</template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="学员归属">
-                            <template scope="scope">
-                                <span class="" :class="{'bg-info':getEmployeeName(scope.row)!='未设定','bg-gray':getEmployeeName(scope.row)=='未设定'}">{{ getEmployeeName(scope.row) }}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="data" label="档案备注">
-                            <template scope="scope">
-                                <p class="ng-binding">{{ scope.row.note }}</p>
-                            </template>
-                        </el-table-column>
-                    </el-table>
-                </div>
-            </div>
-            <div class="panel-footer ">
-                <div class="row ">
-                    <el-pagination class="pull-right" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagination.currentPage" :page-sizes="pagination.pagesizes" :page-size="pagination.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="pagination.total">
-                    </el-pagination>
-                </div>
+                <a @click="lbShowdialog($event,'lb-trash')" :class="{result:refreshData}">
+                    <i class="icon-lock"></i>已封存档案
+                </a>
             </div>
         </div>
-    </div>
+            <div class="row " v-if="localdata.form.view_mode == 'list'">
+                <el-table :data="getTablesData()" stripe>
+                    <el-table-column prop="data" label="操作">
+                        <template scope="scope">
+                            <lb-dropdown :drop-menu-data="localdata.dropDownMenu" :menu-data="scope.row">
+                                <lb-dropdown-button slot="buttonslot" button-class="btn btn-info btn-xs" button-tooltip="操作">
+                                    <i class="fa fa-cog "></i>
+                                    <span class="caret"></span>
+                                </lb-dropdown-button>
+                            </lb-dropdown>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="学员">
+                        <template scope="scope">
+                            <a  @click="handleRouter($event,scope.row)">
+                                <span >
+                                    <i class="fa" :class="{'fa-female':scope.row.sex=='2','fa-male':scope.row.sex=='1'}"></i>
+                                </span>{{ scope.row.student_name }}
+                                <span v-if="scope.row.nickname != ''" >{{ scope.row.nickname }}</span>
+                            </a>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="联系电话">
+                        <template scope="scope">
+                            <span >{{ scope.row.first_tel }}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="年龄">
+                        <template scope="scope">
+                            <span >{{ fromNow(scope.row.birth) }}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="住址">
+                        <template scope="scope">{{ scope.row.home_address }}</template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="学员归属">
+                        <template scope="scope">
+                            <span class="" :class="{'bg-info':getEmployeeName(scope.row)!='未设定','bg-gray':getEmployeeName(scope.row)=='未设定'}">{{ getEmployeeName(scope.row) }}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="data" label="档案备注">
+                        <template scope="scope">
+                            <p >{{ scope.row.note }}</p>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+            <div class="row  footer-panels">
+                <el-pagination class="pull-right" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagination.currentPage" :page-sizes="pagination.pagesizes" :page-size="pagination.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="pagination.total">
+                </el-pagination>
+            </div>
+        </div>
 </template>
 <style >
+.footer-panels {
+    padding: 10px 15px;
+}
+
+.bg-gray {
+    background-color: #cfcfcf;
+    color: #ffffff;
+}
+
+.cell {
+    font-size: 14px;
+}
+
+div.row.wrapper {
+    padding: 0 15px 15px 15px;
+}
 
 .col-md-4 {
     width: 33.33333333%;
 }
+
 .col-md-12 {
     width: 33.33333333%;
 }
--- luban8/src/stores/viewpages.js
+++ luban8/src/stores/viewpages.js
@@ -2,10 +2,12 @@ var routes = []
 import systemsign_in from '~/pages/views/system/sign_in.vue'
 import studentadd from '~/pages/views/student/add.vue'
 import studentlist from '~/pages/views/student/list.vue'
+import studentlist0 from '~/pages/views/student/list0.vue'
 
 let pages = {}
 pages['lb-systemsign_in'] = systemsign_in
 pages['lb-studentadd'] = studentadd
 pages['lb-studentlist'] = studentlist
+pages['lb-studentlist0'] = studentlist0
 
 export default pages
