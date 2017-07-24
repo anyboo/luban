175484b3fa147432584640ca74b68599b5a3e82a
xiaolu <xiaolu_hello@163.com>
2017-07-24 18:26:42 +0800

    提取数据模板

--- luban/webclone/js/background.js
+++ luban/webclone/js/background.js
@@ -15,9 +15,33 @@ setTimeout(function () {
     function getHtml(all, newhtml) {
         let onehtml = {}
         count++
-        onehtml.classList = all.classList
         onehtml.tagName = all.tagName
-        //console.log(onehtml.tagName)
+        let attr = []
+        for (let i = 0; i < all.attributes.length; i++) {
+            let attrvalue = {}
+            attrvalue.name = all.attributes[i].name
+            attrvalue.value = all.attributes[i].value
+            attr.push(attrvalue)
+        }
+        onehtml.attributes = attr
+        if (all.classList) {
+            let classes = []
+            for (let i = 0; i < all.classList.length; i++) {
+                let classvalue = {}
+                classvalue.value = all.classList[i]
+                classes.push(classvalue)
+            }
+            onehtml.classList = classes
+        }
+        if (all.style) {
+            let styles = []
+            for (let i = 0; i < all.style.length; i++) {
+                let stylevalue = {}
+                stylevalue[all.style[i]] = all.style[all.style[i]]
+                styles.push(stylevalue)
+            }
+            onehtml.style = styles
+        }
         onehtml.children = []
         if (all.children) {
             for (var i = 0; i < all.children.length; i++) {
@@ -27,5 +51,5 @@ setTimeout(function () {
         newhtml.push(onehtml)
     }
     getHtml(all[0], html)
-    console.log(html,count)
+    console.log(html, count)
 }, 5000)
