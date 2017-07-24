b523bab1068e0109037d7fecac0e26ef876dda5e
xiaolu <xiaolu_hello@163.com>
2017-07-24 17:00:53 +0800

    循环

--- luban/webclone/js/background.js
+++ luban/webclone/js/background.js
@@ -1,10 +1,31 @@
-setTimeout(function() {
-    console.log(document.all.length, document.styleSheets[0])
+setTimeout(function () {
+    //console.log(document.all.length, document.styleSheets[0])
 
-    for (var i in document.styleSheets[0]) {
-        console.log(i)
-    }
-    //$.get('http://bullstech.cn:9999/api/glassmanage', function(data) {
+    //for (var i in document.styleSheets[0]) {
+    //console.log(i)
+    //}
+    //console.log($("html"))
+    //$.get('http://app.bullstech.cn:9999/api/glassmanage', function(data) {
     //    console.log(data)
     //})
+    var all = $("html")
+    var html = []
+    console.log(all)
+    let count = 0
+    function getHtml(all, newhtml) {
+        let onehtml = {}
+        count++
+        onehtml.classList = all.classList
+        onehtml.tagName = all.tagName
+        //console.log(onehtml.tagName)
+        onehtml.children = []
+        if (all.children) {
+            for (var i = 0; i < all.children.length; i++) {
+                getHtml(all.children[i], onehtml.children)
+            }
+        }
+        newhtml.push(onehtml)
+    }
+    getHtml(all[0], html)
+    console.log(html,count)
 }, 5000)
