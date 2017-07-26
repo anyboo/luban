9ee67f4860d770de622d29aa97393cc07b17fe15
zhangyingyi <1142112046@qq.com>
2017-07-26 17:28:47 +0800

    获取dom

--- luban/webclone/js/background.js
+++ luban/webclone/js/background.js
@@ -124,20 +124,5 @@ setTimeout(function() {
     getHtml(all[0], html)
     lubanclone.html = html
     console.log(html)
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
-    var target = document.querySelector('html');
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            console.log(mutation.type);
-        });
-        console.log(mutations);
-    });
-    var config = {
-        attributes: true,
-        childList: true,
-        characterData: true,
-        subtree: true
 
-    }
-    observer.observe(target, config);
 }, 5000)
