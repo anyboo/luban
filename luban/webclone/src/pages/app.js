7947057a3265d4ad3a73142d0ff2640f4e961c64
sanjin <linzuxin@hotmail.com>
2017-07-27 16:12:03 +0800

    webclone基础工程

--- luban/webclone/src/pages/app.js
+++ luban/webclone/src/pages/app.js
@@ -6,15 +6,46 @@ export default {
             this.$slots.default // 子组件中的阵列
         )
     },
+    data() {
+        return {
+            appdata: {}
+        }
+    },
     mounted() {
-        this.$http.get('http://app.bullstech.cn:8888/webclone/api/eqxiu').then((data) => {
-            console.log(data)
+        this.$http.get('http://app.bullstech.cn:8888/webclone/api/eqxiu').then((req) => {
+            if (req.data) {
+                if (req.data && req.data.count > 0) {
+                    this.unzip(req.data.data[0].info)
+                }
+            }
         })
     },
     props: {
         flag: {
             type: String,
             default: 'h1'
+        }
+    },
+    methods: {
+        unzip(data) {
+            try {
+                JSZip.loadAsync(data, { base64: true })
+                    .then((zip) => {
+                        return zip.file("info.dat").async("string")
+                    })
+                    .then((info) => {
+                        this.appdata = JSON.parse(info)
+                        this.startapp()
+                        console.log(this.appdata)
+                    }, (e) => {
+                        console.log(e)
+                    });
+            } catch (e) {
+                console.log(e)
+            }
+        },
+        startapp() {
+
         }
     }
 }
