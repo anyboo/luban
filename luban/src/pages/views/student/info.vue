4b37b9d0d6249ca89c4c78570a383096213ddcb1
zhangyingyi <1142112046@qq.com>
2017-08-01 17:22:10 +0800

    学员动态记录添加

--- luban/src/pages/views/student/info.vue
+++ luban/src/pages/views/student/info.vue
@@ -38,9 +38,8 @@
                             <p class="text-center">
                                 <span class="text-2x">{{ student.student_name }}</span>
                                 <span>
-                                   
-                                     <i class="fa" :class="{'fa-female ':student.sex=='2','fa-male':student.sex=='1'
-                                        ,'mans':student.sex=='1','woman':student.sex=='2'}"></i>
+                                    <i class="fa" :class="{'fa-female ':student.sex=='2','fa-male':student.sex=='1'
+                                                ,'mans':student.sex=='1','woman':student.sex=='2'}"></i>
                                 </span>
                             </p>
                             <ul class="list-unstyled">
@@ -54,7 +53,7 @@
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-xs-12 col-md-8">
+                        <div class="col-xs-12 col-md-8" style="padding:0;">
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <i class="icon-info"></i> 基础信息</div>
@@ -97,20 +96,20 @@
                 <div class="row">
                     <div class="col-md-4"></div>
                     <div class="col-md-8">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
+                        <div class="panel panel-default" style="margin-right:14px;">
+                            <div class="panel-heading" style="margin-bottom:5px;">
                                 <a class="btn btn-xs btn-default pull-right" @click="rest_save">
                                     <i></i> 保存</a>
                                 <i class="icon-users"></i> 联系人</div>
-                            <table class="table table-striped m-b-none">
-                                <thead>
-                                    <tr style="border-bottom: 1px solid #ccc;background-color:#f4f5f9;">
-                                        <th>电话</th>
-                                        <th class="hidden-xs">称呼</th>
-                                        <th class="hidden-xs">姓名</th>
-                                    </tr>
-                                </thead>
-                            </table>
+                            <!--<table class="table table-striped m-b-none">
+                                        <thead>
+                                            <tr style="border-bottom: 1px solid #ccc;background-color:#f4f5f9;">
+                                                <th>电话</th>
+                                                <th class="hidden-xs">称呼</th>
+                                                <th class="hidden-xs">姓名</th>
+                                            </tr>
+                                        </thead>
+                                    </table>-->
                             <table class="table table-striped m-b-none">
                                 <el-form :model="localdata.form" :rules="rules" label-width="100px" ref="ruleForm">
                                     <el-form-item prop="first_tel">
@@ -145,9 +144,114 @@
                     </div>
                 </div>
             </div>
+            <!--学员动态记录-->
+            <div class="panel panel-default">
+                <div class="panel-heading" tab-link="student.list">
+                    <span class="tab-title">
+                        <i class="fa fa-list"></i> 学员动态记录</span>
+    
+                </div>
+                <div class="panel-body ng-scope">
+                    <div class="wrapper" tab-nav-link="student.list">
+                        <el-collapse v-model="activeName" accordion>
+                            <el-collapse-item title="课程记录" name="1">
+                                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                            </el-collapse-item>
+                            <el-collapse-item title="缴费记录" name="2">
+                                <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                                <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                            </el-collapse-item>
+                            <el-collapse-item title="跟踪回访记录" name="3">
+                                <div>简化流程：设计简洁直观的操作流程；</div>
+                                <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                                <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+                            </el-collapse-item>
+                            <el-collapse-item title="排课记录" name="4">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                              <el-collapse-item title="考勤记录" name="5">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                              <el-collapse-item title="请假记录" name="6">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                              <el-collapse-item title="点评记录" name="7">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                              <el-collapse-item title="成绩记录" name="8">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                             <el-collapse-item title="赛事记录" name="9">
+                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            </el-collapse-item>
+                        </el-collapse>
+                        <!--<ul class="list-group">
+                                <li class="list-group-item ng-scope" style="display: none;">
+                                    <a class="ng-binding" >
+                                        <i class="icon-chemistry"></i> 试课记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" style="display: none;">
+                                    <a class="ng-binding">
+                                        <i class="fa fa-user-md"></i> 评估记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding">
+                                        <i class="fa fa-book"></i> 课程记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-money"></i> 缴费记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding">
+                                        <i class="fa fa-comments"></i> 跟踪回访记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding">
+                                        <i class="fa fa-calendar"></i> 排课记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-calendar-o"></i> 考勤记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-bell-o"></i> 请假记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-comments"></i> 点评记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope">
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-star"></i> 成绩记录</a>
+                                </li>
+                                <li class="list-group-item ng-scope" >
+                                    <a class="ng-binding" >
+                                        <i class="fa fa-flag-o"></i> 赛事记录</a>
+                                </li>
+                            </ul>-->
+                    </div>
+                </div>
+            </div>
         </div>
+    
+        <!--学员动态记录-->
     </div>
 </template>
+<style>
+.col-md-8 {
+    width: 66.66666667%;
+    padding-left: 11px;
+}
+</style>
 <script>
 export default {
     name: 'footer',
@@ -178,6 +282,7 @@ export default {
         }
         return {
             localdata,
+            activeName: '1',
             student: {},
             tables: ['student'],
             uid: '',
