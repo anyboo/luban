f79f55861c134fa0028fa463593952dfb1836c46
xiaolu <xiaolu_hello@163.com>
2017-08-04 19:07:00 +0800

    模块设计

--- luban/src/pages/views/system/module.vue
+++ luban/src/pages/views/system/module.vue
@@ -2,57 +2,90 @@
     <div class="table-block table-box">
         <div class="search">
             <el-row :gutter="12">
-                <el-col :span="6">
+                <el-col :span="6" v-if="textSearch">
                     <el-input placeholder="请输入内容" v-model="input5">
                         <el-select v-model="select" slot="prepend" placeholder="请选择">
-                            <el-option label="餐厅名" value="1"></el-option>
-                            <el-option label="订单号" value="2"></el-option>
-                            <el-option label="用户电话" value="3"></el-option>
+                            <el-option v-for="item in textSearchInfo" :key="item.value" :value="item.value" :label="item.label"></el-option>
                         </el-select>
                         <el-button slot="append" icon="search"></el-button>
                     </el-input>
                 </el-col>
-                <el-col :span="9">
+                <el-col :span="9" v-if="dateSearch">
                     <el-date-picker v-model="value7" type="daterange" align="right" placeholder="选择日期范围" :picker-options="pickerOptions2">
                     </el-date-picker>
                 </el-col>
-                <el-col :span="5">
-                    &nbsp;
+                <el-col :span="5" v-if="groupBtnSearch">
+                    <el-button-group>
+                        <template v-for="item in groupBtnSearchInfo">
+                            <el-button :type="item.type">{{item.label}}</el-button>
+                        </template>
+                    </el-button-group>
                 </el-col>
-                <el-col :span="2">
-                    <el-button type="primary" icon="search" @click="lbShowdialog($event,'lb-newsystemmodule')">搜索</el-button>
+                <el-col :span="2" v-if="singleBtnSearch">
+                    <template v-for="item in singleBtnSearchInfo">
+                        <el-button :type="item.type" :icon="item.icon" @click="lbShowdialog($event,'lb-newsystemmodule')">{{item.label}}</el-button>
+                    </template>
                 </el-col>
             </el-row>
         </div>
         <el-table :data="tableData" stripe border>
-            <el-table-column label="日期" width="180">
-                <template scope="scope">
-                    <el-icon name="time"></el-icon>
-                    <span style="margin-left: 10px">{{ scope.row.date }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
-            <el-table-column label="操作" width="150">
-                <template scope="scope">
-                    <el-dropdown>
-                        <el-button size="mini">
-                            操作
-                            <i class="el-icon-caret-bottom el-icon--right"></i>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item>黄金糕</el-dropdown-item>
-                            <el-dropdown-item>狮子头</el-dropdown-item>
-                            <el-dropdown-item>螺蛳粉</el-dropdown-item>
-                            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-                            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                </template>
-            </el-table-column>
+            <template v-for="item in textTableInfo">
+                <el-table-column :label="item.label" width="280">
+                    <template scope="scope">
+                        <template v-if="item.type=='datetime'">
+                            <el-icon name="time"></el-icon>
+                            <span style="margin-left: 10px">{{ scope.row[item.prop] }}</span>
+                        </template>
+                        <template v-if="item.type=='text'">{{ scope.row[item.prop] }} </template>
+                        <template v-if="item.type=='operation'">
+                            <el-dropdown>
+                                <el-button size="mini">
+                                    {{item.label}}
+                                    <i class="el-icon-caret-bottom el-icon--right"></i>
+                                </el-button>
+                                <template v-for="item in item.fields">
+                                    <el-dropdown-menu slot="dropdown" >
+                                        <el-dropdown-item>{{item}}</el-dropdown-item>
+                                    </el-dropdown-menu>
+                                </template>
+                            </el-dropdown>
+                        </template>
+                        <template v-if="item.type=='textTag'">
+                            <el-tag :type="item.color">{{ scope.row[item.prop]}}</el-tag>
+                        </template>
+                    </template>
+                </el-table-column>
+            </template>
         </el-table>
+        <!-- <el-table :data="tableData" stripe border>
+                                        <el-table-column label="日期" width="180">
+                                            <template scope="scope">
+                                                <el-icon name="time"></el-icon>
+                                                <span style="margin-left: 10px">{{ scope.row.date }}</span>
+                                            </template>
+                                        </el-table-column>
+                                        <el-table-column prop="name" label="姓名" width="180">
+                                        </el-table-column>
+                                        <el-table-column prop="address" label="地址">
+                                        </el-table-column>
+                                        <el-table-column label="操作" width="150">
+                                            <template scope="scope">
+                                                <el-dropdown>
+                                                    <el-button size="mini">
+                                                        操作
+                                                        <i class="el-icon-caret-bottom el-icon--right"></i>
+                                                    </el-button>
+                                                    <el-dropdown-menu slot="dropdown">
+                                                        <el-dropdown-item>黄金糕</el-dropdown-item>
+                                                        <el-dropdown-item>狮子头</el-dropdown-item>
+                                                        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                                                        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                                                        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                                                    </el-dropdown-menu>
+                                                </el-dropdown>
+                                            </template>
+                                        </el-table-column>
+                                    </el-table> -->
         <div class="pagination">
             <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagination.currentPage" :page-sizes="pagination.pagesizes" :page-size="pagination.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="pagination.total">
             </el-pagination>
@@ -93,6 +126,7 @@
 }
 </style>
 <script>
+import module from '~/stores/module.js'
 export default {
     name: 'sclasses',
     data() {
@@ -117,6 +151,7 @@ export default {
             }
         }
         return {
+            module: module.pages.lessonsclasses,
             localdata,
             tables: ['sclasses'],
             input5: '',
@@ -150,23 +185,26 @@ export default {
                 }]
             },
             tableData: [{
-                date: '2016-05-02',
                 name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
+                date: '2016-05-02',
+                tagtext:'陈狗陈'
             }, {
                 date: '2016-05-04',
                 name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
+                tagtext:'陈狗陈'
             }, {
                 date: '2016-05-01',
                 name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
+                tagtext:'陈狗陈'
             }, {
                 date: '2016-05-03',
                 name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
+                tagtext:'陈狗陈'
             }]
         }
+    },
+    mounted() {
+        console.log(this.module)
     },
     computed: {
         changeTeacher() {
@@ -176,6 +214,95 @@ export default {
             }
             return result
         },
+        //搜索
+        textSearchInfo() {
+            let textSearchInfo = []
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'textSearch') {
+                        textSearchInfo = item.fields
+                        break
+                    }
+                }
+            }
+            return textSearchInfo
+        },
+        textSearch() {
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'textSearch') {
+                        return true
+                    }
+                }
+            }
+            return false
+        },
+        dateSearch() {
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'dateSearch') {
+                        return true
+                    }
+                }
+            }
+            return false
+        },
+        singleBtnSearch() {
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'singleBtnSearch') {
+                        return true
+                    }
+                }
+            }
+            return false
+        },
+        singleBtnSearchInfo() {
+            let singleBtnSearchInfo = []
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'singleBtnSearch') {
+                        singleBtnSearchInfo = item.fields
+                        break
+                    }
+                }
+            }
+            return singleBtnSearchInfo
+        },
+        groupBtnSearch() {
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'groupBtnSearch') {
+                        return true
+                    }
+                }
+            }
+            return false
+        },
+        groupBtnSearchInfo() {
+            let groupBtnSearchInfo = []
+            let searchdata = this.module.pageSearch
+            if (searchdata) {
+                for (let item of this.module.pageSearch) {
+                    if (item.type == 'groupBtnSearch') {
+                        groupBtnSearchInfo = item.fields
+                        break
+                    }
+                }
+            }
+            return groupBtnSearchInfo
+        },
+        //表格
+        textTableInfo() {
+            let textTableInfo = this.module.pageTableField
+            return textTableInfo
+        }
     },
     watch: {},
     methods: {
