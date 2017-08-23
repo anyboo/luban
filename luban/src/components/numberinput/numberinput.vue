43b65d91258fb93f1a5f72fb647cfb2617d2f31e
sanjin <linzuxin@hotmail.com>
2017-08-23 10:24:55 +0800

    number数字提交

--- luban/src/components/numberinput/numberinput.vue
+++ luban/src/components/numberinput/numberinput.vue
@@ -1,11 +1,11 @@
 <template>
-    <el-input placeholder="请输入数字" v-model="numbervalue" @blur="changeNum" @change="updateValue">
+    <el-input  placeholder="请输入数字" v-model="numbervalue" @blur="changeNum" @change="updateValue">
         <template slot="append">{{text}}</template>
     </el-input>
 </template>
 <script>
 export default {
-    props: ['value','text'],
+    props: ['value', 'text'],
     name: 'LbNumberinput',
     data() {
         return {
@@ -15,18 +15,28 @@ export default {
     watch: {
         value: function (val) {
             this.numbervalue = Number(val)
-            if (this.numbervalue < 0) {
-                this.numbervalue = 0
+            if (Number.isNaN(this.numbervalue)) {
+                this.numbervalue = ''
+            } else if (this.numbervalue < 0) {
+                this.numbervalue = Math.abs(this.numbervalue)
             }
         }
     },
     methods: {
         changeNum() {
             this.numbervalue = Number(this.numbervalue)
+            if (Number.isNaN(this.numbervalue)) {
+                this.numbervalue = ''
+            } else if (this.numbervalue < 0) {
+                this.numbervalue = Math.abs(this.numbervalue)
+            }
         },
-        updateValue() {
-            if (Number(this.numbervalue) < 0) {
-                this.numbervalue = 0
+        updateValue(val) {
+            this.numbervalue = Number(val)
+            if (Number.isNaN(this.numbervalue)) {
+                this.numbervalue = ''
+            } else if (this.numbervalue < 0) {
+                this.numbervalue = Math.abs(this.numbervalue)
             }
             this.$emit('input', this.numbervalue)
             this.$emit('change', this.numbervalue)
