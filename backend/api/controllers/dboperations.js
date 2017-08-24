48744e8398adaed6e036117cff9c4fa025b39c91
sanjin <linzuxin@hotmail.com>
2017-08-24 17:57:47 +0800

    微信注册

--- backend/api/controllers/dboperations.js
+++ backend/api/controllers/dboperations.js
@@ -1,4 +1,5 @@
 'use strict'
+var crypto = require('crypto');
 var parse = require('co-body')
 var uploadparse = require('co-busboy')
 var MongoClient = require('mongodb').MongoClient
@@ -494,7 +495,32 @@ function ajax(options, body) {
         req.end()
     });
 }
-
+module.exports.wxreg = function* wxreg() {
+    if ('GET' != this.method) return yield next
+    var signature = this.query.signature
+    var timestamp = this.query.timestamp
+    var nonce = this.query.nonce
+    var echostr =this.query.echostr
+    var token = "bullstech"
+  
+    /*  加密/校验流程如下： */
+    //1. 将token、timestamp、nonce三个参数进行字典序排序
+    var array = new Array(token,timestamp,nonce)
+    array.sort()
+    var str = array.toString().replace(/,/g,"")
+  
+    //2. 将三个参数字符串拼接成一个字符串进行sha1加密
+    var sha1Code = crypto.createHash("sha1");
+    var code = sha1Code.update(str,'utf-8').digest("hex")
+  
+    console.log(code,signature,code===signature)
+    //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+    if(code===signature){
+        this.body = echostr
+    }else{
+        this.body = "error"
+    }
+}
 module.exports.wx = function* wx() {
     if ('POST' != this.method) return yield next
     var model = yield parse(this, {
