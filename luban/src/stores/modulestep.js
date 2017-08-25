deb95b3123b9a83f07df1d2b7bbc784766cee9eb
xiaolu <xiaolu_hello@163.com>
2017-08-25 09:14:27 +0800

    优化

--- luban/src/stores/modulestep.js
+++ luban/src/stores/modulestep.js
@@ -1,32 +1,7 @@
-import sclassesform from './formmodules/sclassesform.js'
-import campusform from './formmodules/campusform.js'
-import leavesform from './formmodules/leavesform.js'
-import suspendform from './formmodules/suspendform.js'
-import inquiryform from './formmodules/inquiryform.js'
-import recordingform from './formmodules/recordingform.js'
-import employeeform from './formmodules/employeeform.js'
-import classesform from './formmodules/classesform.js'
-import flowform from './formmodules/flowform.js'
-import studentregion from './formmodules/studentregion.js'
-import studentpurpose from './formmodules/studentpurpose.js'
-import orderform from './formmodules/orderform.js'
-import arrangeform from './formmodules/arrangeform.js'
-import arrangeeditform from './formmodules/arrangeeditform.js'
+import attendancedialog from './stepmodules/attendancedialog.js'
+
 
 let pages = {}
-pages['sclassesform'] = sclassesform
-pages['campusform'] = campusform
-pages['leavesform'] = leavesform
-pages['suspendform'] = suspendform
-pages['inquiryform'] = inquiryform
-pages['recordingform'] = recordingform
-pages['employeeform'] = employeeform
-pages['classesform'] = classesform
-pages['flowform'] = flowform
-pages['studentregion'] = studentregion
-pages['studentpurpose'] = studentpurpose
-pages['orderform'] = orderform
-pages['arrangeform'] = arrangeform
-pages['arrangeeditform'] = arrangeeditform
+pages['attendancedialog'] = attendancedialog
 
 export default pages
