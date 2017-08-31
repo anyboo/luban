cefd0db8cff32052c0878b2ca9648ac0d3dd3d11
sanjin <linzuxin@hotmail.com>
2017-08-31 11:19:54 +0800

    报名

--- luban8/src/pages/dialogs/lesson/cate.vue
+++ luban8/src/pages/dialogs/lesson/cate.vue
@@ -1,28 +1,25 @@
 <template>
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
         <div class="modal-content">
-            <div page-controller="lesson_cate" >
-                <div class="modal-header">
-                    <button class="close" type="button" @click="lbClosedialog($event)">
-                        <span aria-hidden="true">×</span>
-                        <span class="sr-only">关闭</span>
-                    </button>
-                    <h3 class="modal-title">
-                        <i class="fa fa-book"></i>课程分类设置
-                    </h3>
-                </div>
-                <div class="modal-body">
-                    <p>
-                        <a class="btn btn-default" @click="handleClick">
-                            <i class="fa fa-plus"></i>新增大类
-                        </a>
-                    </p>
-                    <lb-listtree :tree-data="getreeData" ref="listtree">
-                    </lb-listtree>
-                </div>
-                <div class="modal-footer text-center">
-                    <button class="btn btn-warning" @click="lbClosedialog($event)">关闭</button>
-                </div>
+            <div class="modal-header">
+                <button class="close" type="button" @click="lbClosedialog()">
+                    <span aria-hidden="true">×</span>
+                    <span class="sr-only">关闭</span>
+                </button>
+                <h3 class="modal-title">
+                    <i class="fa fa-book"></i>课程分类设置</h3>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <a class="btn btn-default" @click="handleClick">
+                        <i class="fa fa-plus"></i>新增大类
+                    </a>
+                </p>
+                <lb-listtree :tree-data="getreeData" ref="listtree">
+                </lb-listtree>
+            </div>
+            <div class="modal-footer text-center">
+                <button class="btn btn-warning" @click="lbClosedialog($event)">关闭</button>
             </div>
         </div>
     </div>
@@ -38,7 +35,7 @@ export default {
         }
         return {
             localdata,
-              model: 'cate',
+            model: 'cate',
             tables: ['cate']
         }
     },
