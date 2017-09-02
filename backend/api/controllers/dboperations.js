cc850c207751001e342ee1d004850aeb0295b62b
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-09-02 18:12:20 +0800

    fuwuduan

--- backend/api/controllers/dboperations.js
+++ backend/api/controllers/dboperations.js
@@ -525,7 +525,7 @@ function ajaxhttp(options, body) {
 
 module.exports.wxregpost = function* wxregpost() {
     if ('POST' != this.method) return yield next
-    var model = this
+    var model = this.request.body
     var signature = this.query.signature
     var timestamp = this.query.timestamp
     var nonce = this.query.nonce
