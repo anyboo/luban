a50fd9c362691dba6f754a8e9a94bfb0a4d53a97
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-09-04 18:17:40 +0800

    111

--- backend/api/controllers/dboperations.js
+++ backend/api/controllers/dboperations.js
@@ -638,36 +638,35 @@ module.exports.wxmenus = function* wxmenus() {
     access_info = yield ajax(access_options)
     console.log(access_info)
     let wx_item = {
-        "button": [
-            {
-                "type": "click",
-                "name": "今日歌曲",
-                "key": "V1001_TODAY_MUSIC"
+        "button": [{
+            "type": "click",
+            "name": "今日歌曲",
+            "key": "V1001_TODAY_MUSIC"
+        },
+        {
+            "name": "菜单",
+            "sub_button": [{
+                "type": "view",
+                "name": "搜索",
+                "url": "http://www.soso.com/"
             },
             {
-                "name": "菜单",
-                "sub_button": [
-                    {
-                        "type": "view",
-                        "name": "搜索",
-                        "url": "http://www.soso.com/"
-                    },
-                    {
-                        "type": "miniprogram",
-                        "name": "wxa",
-                        "url": "http://mp.weixin.qq.com",
-                        "appid": "wx286b93c14bbf93aa",
-                        "pagepath": "pages/lunar/index"
-                    },
-                    {
-                        "type": "click",
-                        "name": "赞一下我们",
-                        "key": "V1001_GOOD"
-                    }]
+                "type": "miniprogram",
+                "name": "wxa",
+                "url": "http://mp.weixin.qq.com",
+                "appid": "wx286b93c14bbf93aa",
+                "pagepath": "pages/lunar/index"
+            },
+            {
+                "type": "click",
+                "name": "赞一下我们",
+                "key": "V1001_GOOD"
             }]
+        }]
     }
-    console.log(wx_item)
+/*     console.log(wx_item) */
     let body = querystring.stringify(wx_item)
+    console.log(body)
     let options = {
         hostname: 'api.weixin.qq.com',
         port: 443,
