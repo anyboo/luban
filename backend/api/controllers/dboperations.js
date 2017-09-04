f81c69262641720a9581e0b4545a38224b0f3578
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-09-04 17:38:03 +0800

    11

--- backend/api/controllers/dboperations.js
+++ backend/api/controllers/dboperations.js
@@ -637,65 +637,41 @@ module.exports.wxmenus = function* wxmenus() {
     let access_info = {}
     access_info = yield ajax(access_options)
     console.log(access_info)
-    let wx_item = {
-        "button": [
-            {
-                "name": "扫码", 
-                "sub_button": [
-                    {
-                        "type": "scancode_waitmsg", 
-                        "name": "扫码带提示", 
-                        "key": "rselfmenu_0_0", 
-                        "sub_button": [ ]
-                    }, 
-                    {
-                        "type": "scancode_push", 
-                        "name": "扫码推事件", 
-                        "key": "rselfmenu_0_1", 
-                        "sub_button": [ ]
+    let wx_item = { 
+        "is_menu_open": 1, 
+        "selfmenu_info": { 
+            "button": [ 
+                { 
+                    "type": "click", 
+                    "name": "今日歌曲", 
+                    "key": "V1001_TODAY_MUSIC"
+                }, 
+                { 
+                    "name": "菜单", 
+                    "sub_button": { 
+                        "list": [ 
+                            { 
+                                "type": "view", 
+                                "name": "搜索", 
+                                "url": "http://www.soso.com/"
+                            }, 
+                            { 
+                                "type": "view", 
+                                "name": "视频", 
+                                "url": "http://v.qq.com/"
+                            }, 
+                            { 
+                                "type": "click", 
+                                "name": "赞一下我们", 
+                                "key": "V1001_GOOD"
+                            }
+                        ]
                     }
-                ]
-            }, 
-            {
-                "name": "发图", 
-                "sub_button": [
-                    {
-                        "type": "pic_sysphoto", 
-                        "name": "系统拍照发图", 
-                        "key": "rselfmenu_1_0", 
-                       "sub_button": [ ]
-                     }, 
-                    {
-                        "type": "pic_photo_or_album", 
-                        "name": "拍照或者相册发图", 
-                        "key": "rselfmenu_1_1", 
-                        "sub_button": [ ]
-                    }, 
-                    {
-                        "type": "pic_weixin", 
-                        "name": "微信相册发图", 
-                        "key": "rselfmenu_1_2", 
-                        "sub_button": [ ]
-                    }
-                ]
-            }, 
-            {
-                "name": "发送位置", 
-                "type": "location_select", 
-                "key": "rselfmenu_2_0"
-            },
-            {
-               "type": "media_id", 
-               "name": "图片", 
-               "media_id": "MEDIA_ID1"
-            }, 
-            {
-               "type": "view_limited", 
-               "name": "图文消息", 
-               "media_id": "MEDIA_ID2"
-            }
-        ]
-    }
+                }
+            ]
+        }
+     }
+     
     let body = querystring.stringify(wx_item)
     let options = {
         hostname: 'api.weixin.qq.com',
