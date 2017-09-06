1116ad5973ae82770605d47979b55a4a1f89fb9c
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-09-06 16:49:58 +0800

    客服

--- backend/api/app.js
+++ backend/api/app.js
@@ -28,6 +28,7 @@ app.use(route.get('/:db/wxqrcode/:id', wx.wxqrcode))
 //微信服务
 
 app.use(route.post('/wxservice/:type', wxserver.wxservice))
+app.use(route.post('/getwxserver/', wxserver.getwxserver))
 //短信
 app.use(route.post('/smssend/', sms.smssend))
 app.use(route.get('/getsmssend/', sms.getsmssend))
--- backend/api/controllers/wx/wxserver.js
+++ backend/api/controllers/wx/wxserver.js
@@ -41,7 +41,7 @@ module.exports.wxservice = function* wxservice(type) {
     let options = {
         hostname: 'api.weixin.qq.com',
         port: 443,
-        path: '/customservice/kfaccount/'+type+'?access_token=' + access_info.access_token,
+        path: '/customservice/kfaccount/' + type + '?access_token=' + access_info.access_token,
         method: 'POST',
         json: true,
         headers: {
@@ -52,6 +52,22 @@ module.exports.wxservice = function* wxservice(type) {
     let wxinfo = yield net.ajax(options, body)
     this.body = yield wxinfo
 }
-module.exports.wxserput = function* wxserput() {
-
+module.exports.getwxserver = function* getwxserver() {
+    if ('GET' != this.method) return yield next
+    let access_options = {
+        hostname: 'api.weixin.qq.com',
+        port: 443,
+        path: '/cgi-bin/token?grant_type=client_credential&appid=wx30db7ec1537d9afc&secret=6a3a743d25071d06f82153d029dee8cf',
+        method: 'GET',
+    }
+    let access_info = {}
+    access_info = yield net.ajax(access_options)
+    let options = {
+        hostname: 'api.weixin.qq.com',
+        port: 443,
+        path: '/cgi-bin/customservice/getkflist?access_token='+access_info.access_token,
+        method: 'GET',
+    }
+    access_smssend = yield net.ajax(options)
+    this.body = yield access_smssend
 }
