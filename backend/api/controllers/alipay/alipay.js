fe65160229684ef4282d18b8179a963ff05c49a6
18059842153 <501617031@qq.com>
2017-09-13 10:26:02 +0800

    ss

---
+++ backend/api/controllers/alipay/alipay.js
@@ -0,0 +1,22 @@
+'use strict'
+var crypto = require('crypto')
+var parse = require('co-body')
+var uploadparse = require('co-busboy')
+var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID
+const jwt = require('jsonwebtoken')
+var fs = require('fs')
+var Buffer = require('buffer').Buffer
+var path = require('path')
+const SMSClient = require('./smsclient.js')
+const accessKeyId = 'ACSFUX7fLUMpBZM1'
+const secretAccessKey = 'qsGNrvuGnu'
+const queueName = 'Alicom-Queue-1420938370661882-'
+const smsdb = 'lubansms'
+const querystring = require('querystring')
+var net = require('../../unit/net')
+var dbunit = require('../../unit/db')
+
+module.exports.alipay = function* alipay() {
+
+}
