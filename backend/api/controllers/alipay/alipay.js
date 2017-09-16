0f175c1e695efea9d415f29278346e2c7888e718
18059842153 <501617031@qq.com>
2017-09-16 11:20:14 +0800

    zhuanma

--- backend/api/controllers/alipay/alipay.js
+++ backend/api/controllers/alipay/alipay.js
@@ -121,21 +121,6 @@ module.exports.alipay = function* alipay() {
     }
 
     aliinfo = yield net.ajax(ali_options, body, true)
-    var http = require("http")
-    var url = 'aliinfo'
-    http.get(url, function(res){
-        var arrBuf = []
-        var bufLength = 0
-        res.on("data", function(chunk){
-            arrBuf.push(chunk)
-            bufLength += chunk.length
-        })
-        .on("end", function(){
-            // arrBuf是个存byte数据块的数组，byte数据块可以转为字符串，数组可不行
-            // bufferhelper也就是替你计算了bufLength而已 
-            var chunkAll = Buffer.concat(arrBuf, bufLength);   
-            var strJson = iconv.decode(chunkAll,'gb2312'); // 汉字不乱码
-          this.body = strJson
-        });
-    });
-    }
+    aliinfo =iconv.encode(aliinfo,'utf-8')
+    this.body = aliinfo
+}
