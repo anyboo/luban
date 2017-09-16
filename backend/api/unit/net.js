7d160efab02f5eebf0426c40fa7449ff0b0e00e0
18059842153 <501617031@qq.com>
2017-09-16 16:47:03 +0800

    node

--- backend/api/unit/net.js
+++ backend/api/unit/net.js
@@ -1,6 +1,37 @@
 const https = require('https')
 const http = require('http')
+var iconv = require('iconv-lite')
 
+module.exports.ajaxgb2312 = function (options, body) {
+    return new Promise(function (resolve) {
+        var arrBuf = [];
+        var bufLength = 0;
+        const req = https.request(options, (res) => {
+            //res.setEncoding('utf8')
+            res.on('data', (d) => {
+                arrBuf.push(d);
+                bufLength += d.length;
+            })
+        })
+        if (options.method == 'POST') {
+            req.write(body)
+        }
+        req.on('error', (e) => {
+            console.error(e)
+        })
+        req.on('end', () => {
+            try {
+                var chunkAll = Buffer.concat(arrBuf, bufLength)
+                console.log(chunkAll) 
+                var strJson = iconv.decode(chunkAll,'gb2312')
+                resolve(chunkAll)
+            } catch (e) {
+                console.log(e)
+            }
+        })
+        req.end()
+    })
+}
 module.exports.ajax = function (options, body, html) {
     return new Promise(function (resolve) {
         let data = ''
