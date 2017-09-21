cdfe417f6f448daa2ade9bd98f45ed31f34ea7d5
sanjin <linzuxin@hotmail.com>
2017-09-21 11:17:27 +0800

    学员详情

--- luban8/src/pages/views/student/info.vue
+++ luban8/src/pages/views/student/info.vue
@@ -42,7 +42,7 @@
                             </li>
                              <li>
                                 <label class="field">账号余额:</label>
-                                <span>{{ student.nickname }}</span>
+                                <span>{{ student.amount }}</span>
                             </li>
                             <li class="m-t-xs">
                                 <label class="field">学员归属:</label>
